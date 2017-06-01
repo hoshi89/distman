a06999ffe68b2afcd7cd964bba9ca1c0d702cd3a
Robin Engström <robin.engstrom@op5.com>
2017-06-01 09:34:42 +0200

    module.c: added notification handling concept and check handling concept
    Signed-off-by: Robin Engström <robin.engstrom@op5.com>

--- src/module.c
+++ src/module.c
@@ -1,10 +1,14 @@
 #include <zookeeper/zookeeper.h>
 #include <naemon/naemon.h>
+#include <sys/wait.h>
 
 NEB_API_VERSION(CURRENT_NEB_API_VERSION)
 
 nebmodule *neb_handle;
+int node_flags = 0;
 
+/* Some temporary test config */
+/* TODO: Read config from file */
 static zhandle_t *zh;
 const char app_name[] = "distman";
 const char cluster_name[] = "master";
@@ -21,7 +25,6 @@ void watcher (zhandle_t *zzh, int type, int state, const char *path, void *watch
         /* Add a new watch so we can detect disconnects etc */
         zoo_get_children(zh, "/", 1, NULL);
         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Child event!");
-
     } else if (type == ZOO_SESSION_EVENT) {
         if (state == ZOO_CONNECTED_STATE) {
             nm_log(NSLOG_INFO_MESSAGE, "DistMan: Connected to ZooKeeper server!");
@@ -35,6 +38,7 @@ void watcher (zhandle_t *zzh, int type, int state, const char *path, void *watch
     }
 
     if (state == ZOO_EXPIRED_SESSION_STATE) {
+        /* TODO: Reconnect */
         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Expired session state");
     } else if (state == ZOO_AUTH_FAILED_STATE) {
         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Auth failed state");
@@ -48,20 +52,6 @@ void watcher (zhandle_t *zzh, int type, int state, const char *path, void *watch
         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Unknown ZooKeeper state! %d", state);
     }
 }
-
-int handle_host_check (int event_type, void *data) {
-    nebstruct_host_check_data *ds = (nebstruct_host_check_data *)data;
-    if (ds->type == NEBTYPE_HOSTCHECK_INITIATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Overriding host check");
-        return NEBERROR_CALLBACKOVERRIDE;
-    }
-    return NEB_OK;
-}
-
-/* Some temporary test config */
-/* TODO: Read config from file */
-const char app_name[] = "distman";
-const char cluster_name[] = "master";
 
 char *rc_to_string(int rc) {
     switch (rc) {
@@ -86,59 +76,139 @@ char *rc_to_string(int rc) {
     }
 }
 
-void watcher (zhandle_t *zzh, int type, int state, const char *path, void *watcherCtx) {
-    if (type == ZOO_CREATED_EVENT) {
-        zoo_exists(zh, "/robin", 1, NULL);
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Created event!");
-    } else if (type == ZOO_DELETED_EVENT) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Deleted event!");
-    } else if (type == ZOO_CHANGED_EVENT) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Changed event!");
-    } else if (type == ZOO_CHILD_EVENT) {
-        /* Add a new watch so we can detect disconnects etc */
-        zoo_get_children(zh, "/", 1, NULL);
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Child event!");
+check_result *copy_check_result(check_result *src) {
+    check_result *cr = nm_calloc(1, sizeof(*cr));
+    init_check_result(cr);
+    cr->object_check_type = src->object_check_type;
+    cr->host_name = src->host_name ? nm_strdup(src->host_name) : NULL;
+    cr->service_description = src->service_description ? nm_strdup(src->service_description) : NULL;
+    cr->check_type = src->check_type;
+    cr->check_options = src->check_options;
+    cr->scheduled_check = src->scheduled_check;
+    cr->output_file = src->output_file ? nm_strdup(src->output_file) : NULL;
+    cr->output_file_fp = src->output_file_fp;
+    cr->latency = src->latency;
+    cr->start_time = src->start_time;
+    cr->finish_time = src->finish_time;
+    cr->early_timeout = src->early_timeout;
+    cr->exited_ok = src->exited_ok;
+    cr->return_code = src->return_code;
+    cr->output = src->output ? nm_strdup(src->output) : NULL;
+    return cr;
+}
 
-    } else if (type == ZOO_SESSION_EVENT) {
-        if (state == ZOO_CONNECTED_STATE) {
-            nm_log(NSLOG_INFO_MESSAGE, "DistMan: Connected to ZooKeeper server!");
-        } else if (state == ZOO_CONNECTING_STATE) {
-            nm_log(NSLOG_INFO_MESSAGE, "DistMan: Trying to connect to ZooKeeper server...");
-        }
-    } else if (type == ZOO_NOTWATCHING_EVENT) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Notwatching event!");
-    } else {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Unknown ZooKeeper event!");
-    }
+static void handle_host_check_work(wproc_result *wpres, void *arg, int flags) {
+	check_result *cr = (check_result *)arg;
+	struct host *hst;
 
-    if (state == ZOO_EXPIRED_SESSION_STATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Expired session state");
-    } else if (state == ZOO_AUTH_FAILED_STATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Auth failed state");
-    } else if (state == ZOO_CONNECTING_STATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Connecting state");
-    } else if (state == ZOO_ASSOCIATING_STATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Associating state");
-    } else if (state == ZOO_CONNECTED_STATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Connected state");
-    } else {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Unknown ZooKeeper state! %d", state);
-    }
+	/* decrement the number of host checks still out there... */
+	if (currently_running_host_checks > 0)
+		currently_running_host_checks--;
+
+	hst = find_host(cr->host_name);
+	if (hst && wpres) {
+		hst->is_executing = FALSE;
+		memcpy(&cr->rusage, &wpres->rusage, sizeof(wpres->rusage));
+		cr->start_time.tv_sec = wpres->start.tv_sec;
+		cr->start_time.tv_usec = wpres->start.tv_usec;
+		cr->finish_time.tv_sec = wpres->stop.tv_sec;
+		cr->finish_time.tv_usec = wpres->stop.tv_usec;
+		if (WIFEXITED(wpres->wait_status)) {
+			cr->return_code = WEXITSTATUS(wpres->wait_status);
+		} else {
+			cr->return_code = STATE_UNKNOWN;
+		}
+
+		if (wpres->outstd && *wpres->outstd) {
+			cr->output = nm_strdup(wpres->outstd);
+		} else if (wpres->outerr && *wpres->outerr) {
+			nm_asprintf(&cr->output, "(No output on stdout) stderr: %s", wpres->outerr);
+		} else {
+			cr->output = NULL;
+		}
+
+		cr->early_timeout = wpres->early_timeout;
+		cr->exited_ok = wpres->exited_ok;
+		cr->engine = NULL;
+		cr->source = wpres->source;
+
+        /*
+         * Put check result on the results queue
+         */
+         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Handling host check %s", cr->host_name);
+
+		process_check_result(cr);
+	}
+	free_check_result(cr);
+	free(cr);
 }
 
 int handle_host_check (int event_type, void *data) {
     nebstruct_host_check_data *ds = (nebstruct_host_check_data *)data;
     if (ds->type == NEBTYPE_HOSTCHECK_INITIATE) {
         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Overriding host check");
+        check_result *cr = copy_check_result((check_result *)ds->check_result_ptr);
+        wproc_run_callback(ds->command_line, ds->timeout, handle_host_check_work, cr, NULL);
         return NEBERROR_CALLBACKOVERRIDE;
     }
 	return NEB_OK;
 }
 
+static void handle_service_check_work(wproc_result *wpres, void *arg, int flags) {
+	check_result *cr = (check_result *)arg;
+	if(wpres) {
+		memcpy(&cr->rusage, &wpres->rusage, sizeof(wpres->rusage));
+		cr->start_time.tv_sec = wpres->start.tv_sec;
+		cr->start_time.tv_usec = wpres->start.tv_usec;
+		cr->finish_time.tv_sec = wpres->stop.tv_sec;
+		cr->finish_time.tv_usec = wpres->stop.tv_usec;
+		if (WIFEXITED(wpres->wait_status)) {
+			cr->return_code = WEXITSTATUS(wpres->wait_status);
+		} else {
+			cr->return_code = STATE_UNKNOWN;
+		}
+
+		if (wpres->outstd && *wpres->outstd) {
+			cr->output = nm_strdup(wpres->outstd);
+		} else if (wpres->outerr && *wpres->outerr) {
+			nm_asprintf(&cr->output, "(No output on stdout) stderr: %s", wpres->outerr);
+		} else {
+			cr->output = NULL;
+		}
+
+		cr->early_timeout = wpres->early_timeout;
+		cr->exited_ok = wpres->exited_ok;
+		cr->engine = NULL;
+		cr->source = wpres->source;
+
+        /*
+         * Put the check result on the results queue
+         */
+         nm_log(NSLOG_INFO_MESSAGE, "DistMan: Handling service check %s", cr->host_name);
+
+		process_check_result(cr);
+	}
+	free_check_result(cr);
+	free(cr);
+}
+
 int handle_service_check (int event_type, void *data) {
     nebstruct_service_check_data *ds = (nebstruct_service_check_data *)data;
     if (ds->type == NEBTYPE_SERVICECHECK_INITIATE) {
-        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Overriding service check");
+        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Overriding service check %s", ds->host_name);
+        check_result *cr = copy_check_result((check_result *)ds->check_result_ptr);
+        wproc_run_callback(ds->command_line, ds->timeout, handle_service_check_work, cr, NULL);
+        return NEBERROR_CALLBACKOVERRIDE;
+    }
+    return NEB_OK;
+}
+
+int handle_notification (int event_type, void *data) {
+    nebstruct_notification_data *ds = (nebstruct_notification_data *)data;
+    if (ds->type == NEBTYPE_NOTIFICATION_START) {
+        /* If I am the leader node, allow notification to proceed */
+        /* If I am NOT the leader, override this notification */
+        nm_log(NSLOG_INFO_MESSAGE, "DistMan: Overriding notification");
         return NEBERROR_CALLBACKOVERRIDE;
     }
     return NEB_OK;
@@ -148,18 +218,21 @@ int nebmodule_init (int flags, char *arg, nebmodule *handle) {
 
     char buffer[1024];
     int rc = 0;
+    int i = 0;
 
     neb_handle = handle;
 
     event_broker_options = ~0; /* force settings to "everything" */
     neb_register_callback(NEBCALLBACK_SERVICE_CHECK_DATA, neb_handle, 0, handle_service_check);
     neb_register_callback(NEBCALLBACK_HOST_CHECK_DATA, neb_handle, 0, handle_host_check);
+    neb_register_callback(NEBCALLBACK_NOTIFICATION_DATA, neb_handle, 0, handle_notification);
 
     zh = zookeeper_init("127.0.0.1:2181", watcher, 10000, 0, 0, 0);
 
     /*
      * Set up necessary paths
      * TODO: Make ACL more secure
+     * TODO: Leader election
      */
     sprintf(buffer, "/%s", app_name);
     rc = zoo_create(zh, buffer, "value", 5, &ZOO_OPEN_ACL_UNSAFE, 0, buffer, sizeof(buffer)-1);
@@ -185,15 +258,29 @@ int nebmodule_init (int flags, char *arg, nebmodule *handle) {
         return -1;
     }
 
-    // 2. Determine if I am the leader
-    // 3. If yes, set leader to true
-    // 4. If no, create a watch on the node I created-1
+    /* Determine the leader node */
+    struct Stat stat;
+    int buflen = sizeof(buffer);
+    rc = zoo_get(zh, buffer, 0, buffer, &buflen, &stat);
+    if (rc != ZOK) {
+        nm_log(NSLOG_INFO_MESSAGE, "DistMan: failed to get data from session node, reason: %s",
+            rc_to_string(rc));
+        return -1;
+    }
 
-    //zoo_get_children(zh, "/", 1, NULL);
-    //struct Stat stat;
-    //rc = zoo_get(zh, "/test", 1, buffer, &size, NULL);
-    //nm_log(NSLOG_INFO_MESSAGE, "DistMan: RC IS %d", rc);
-    zoo_exists(zh, "/robin", 1, NULL);
+    sprintf(buffer, "/%s/%s", app_name, cluster_name);
+    struct String_vector children;
+    rc = zoo_get_children(zh, buffer, 0, &children);
+    if (rc != ZOK) {
+        nm_log(NSLOG_INFO_MESSAGE, "DistMan: failed to get data children, reason: %s",
+            rc_to_string(rc));
+        return -1;
+    }
+
+    for (i = 0; i < children.count; i++) {
+        printf("Empheral node owner: %s\n", children.data[i]);
+    }
+
     return 0;
 }
 
